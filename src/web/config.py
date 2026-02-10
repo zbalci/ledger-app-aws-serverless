b13a1b8d2a1a3b56653ae2f0e7d46f587959d560
Zeki <zkbalci@gmail.com>
2026-02-10 05:54:24 +0300

    lambda and apigateway cfn templates added

--- src/web/config.py
+++ src/web/config.py
@@ -1,12 +1,6 @@
-# import boto3
+import boto3
 
 class Config:
-    # SESSION_TYPE = 'dynamodb'
-    # SESSION_DYNAMODB = boto3.resource('dynamodb')
-    # SESSION_DYNAMODB_TABLE = 'flask-sessions'
-
-    SESSION_TYPE = 'filesystem'
-    SESSION_FILE_DIR = "/tmp/flask_session"
-    SESSION_PERMANENT = False
-    SESSION_USE_SIGNER = True
-    SESSION_KEY_PREFIX = 'flask:'
+    SESSION_TYPE = 'dynamodb'
+    SESSION_DYNAMODB = boto3.resource('dynamodb')
+    SESSION_DYNAMODB_TABLE = 'flask-sessions'
--- src/web/lambda_function.py
+++ src/web/lambda_function.py
@@ -4,12 +4,12 @@ from config import Config  # Eğer Config gerekiyorsa ekle
 # Flask uygulamasını oluştur
 app = create_app(Config)
 
-if __name__ == '__main__':
-   context = ('cert/fullchain.pem', 'cert/privkey.pem')
-   app.run(debug=True, host='0.0.0.0', port=8443, ssl_context=context)
+# if __name__ == '__main__':
+#    context = ('cert/fullchain.pem', 'cert/privkey.pem')
+#    app.run(debug=True, host='0.0.0.0', port=8443, ssl_context=context)
 
-# # Flask'ı Lambda ile çalışacak şekilde yapılandırın
-# # AWS Lambda ortamında WSGI'yi çalıştıran serverless_wsgi modülünü kullanın
-# def lambda_handler(event, context):
-#    from serverless_wsgi import handle_request
-#    return handle_request(app, event, context)
+# Flask'ı Lambda ile çalışacak şekilde yapılandırın
+# AWS Lambda ortamında WSGI'yi çalıştıran serverless_wsgi modülünü kullanın
+def lambda_handler(event, context):
+   from serverless_wsgi import handle_request
+   return handle_request(app, event, context)
